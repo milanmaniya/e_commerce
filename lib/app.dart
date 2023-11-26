0e779433c265f1cb1bb1de0b906498b5f1f9b1d5
milanmaniya <milanmaniya2035@gmail.com>
2023-11-26 17:43:49 +0530

    first commit

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:e_commerce/utils/theme/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      darkTheme: TAppTheme.darkTheme,
+      theme: TAppTheme.lightTheme,
+    );
+  }
+}
