b7f583016030fd4db063e2ea3806a15f4b255513
milanmaniya <milanmaniya2035@gmail.com>
2023-11-27 18:09:51 +0530

    pricing calculation complete

---
+++ lib/utils/helper/pricing_calculator.dart
@@ -0,0 +1,30 @@
+class TPricingCalculator {
+  static double calculateTotalPrice(double productPrice, String location) {
+    double taxRate = getTaxRateForLocation(location);
+    double taxAmount = productPrice * taxRate;
+
+    double shippingCost = getShittpingCost(location);
+
+    double totalPrice = productPrice + taxAmount + shippingCost;
+    return totalPrice;
+  }
+
+  static String calculateShippingCost(double productPrice, String location) {
+    double shippingCost = getShittpingCost(location);
+    return shippingCost.toStringAsFixed(2);
+  }
+
+  String calculateTax(double productPrice, String location) {
+    double taxRate = getTaxRateForLocation(location);
+    double taxAmount = productPrice * taxRate;
+    return taxAmount.toStringAsFixed(2);
+  }
+
+  static double getTaxRateForLocation(String location) {
+    return 0.10; // tax rate is 10%
+  }
+
+  static double getShittpingCost(String location) {
+    return 5.00; // shipping cost is $5
+  }
+}
