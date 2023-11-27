c283dc4d89bf3b4baf89625db595313ccc1f1c09
milanmaniya <milanmaniya2035@gmail.com>
2023-11-27 11:19:44 +0530

    text string complete

---
+++ lib/utils/constant/text_strings.dart
@@ -0,0 +1,18 @@
+class TTexts {
+  // on boarding text
+  static const String onBordingTitle1 = "Choose your product";
+  static const String onBordingTitle2 = "Select Payment Method";
+  static const String onBordingTitle3 = "Deliver at your door step";
+
+  // on boarding subtitle
+  static const String onBordingSubtitleTitle1 =
+      "Welcome to a World of Limitless Choices - Your Perfect Product Awaits!";
+  static const String onBordingSubtitleTitle2 =
+      "For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!";
+  static const String onBordingSubtitleTitle3 =
+      "From Our Doorstep to Yours - Swift, Secure, and Contactless Delivery!";
+
+  // home
+  static const String homeAppBarTitle = "Good day for shopping";
+  static const String homeAppBarSubTitle = "Milan Maniya";
+}
