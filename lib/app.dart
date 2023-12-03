db94692369aa92045bde31f4bab27f58f0f09c75
milanmaniya <milanmaniya2035@gmail.com>
2023-12-03 18:34:33 +0530

    splash screen complete

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,37 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:e_commerce/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.system,
       darkTheme: TAppTheme.darkTheme,
       theme: TAppTheme.lightTheme,
+      home: AnimatedSplashScreen(
+        splash: 'assets/logos/TRENDY-logos_black.png',
+        centered: true,
+        splashIconSize: 550,
+        animationDuration: const Duration(
+          seconds: 2,
+        ),
+        duration: 3000,
+        nextScreen: const Scaffold(
+          body: Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 30,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
