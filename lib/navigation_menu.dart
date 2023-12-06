8efb51bf77c2a923d16dc3ee983cdb39cd1ddf24
milanmaniya <milanmaniya2035@gmail.com>
2023-12-06 10:41:14 +0530

    bottom navigation bar ui completed

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,33 +1,61 @@
+import 'package:e_commerce/utils/helper/helper_function.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:icons_plus/icons_plus.dart';
 
 class NavigationMenu extends StatelessWidget {
   const NavigationMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(NavigationMenuController());
+
+    final dark = THelperFunction.isDarkMode(context);
+
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: 0,
-        elevation: 0,
-        
-        destinations: [
-          Container(
-            color: Colors.black,
-          ),
-          Container(
-            color: Colors.green,
-          ),
-          Container(
-            color: Colors.red,
-          ),
-          Container(
-            color: Colors.orange,
-          ),
-        ],
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          height: 75,
+          elevation: 0,
+          backgroundColor: dark ? Colors.black : Colors.white,
+          indicatorColor: dark
+              ? Colors.white.withOpacity(0.1)
+              : Colors.black.withOpacity(0.1),
+          onDestinationSelected: (value) =>
+              controller.selectedIndex.value = value,
+          selectedIndex: controller.selectedIndex.value,
+          destinations: const [
+            NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
+            NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
+            NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'), 
+          ],
+        ),
       ),
-      body: Container(
-        color: Colors.purple,
+      body: Obx(
+        () => Center(
+          child: controller.screenList[controller.selectedIndex.value],
+        ),
       ),
     );
   }
+}
+
+class NavigationMenuController extends GetxController {
+  final selectedIndex = 0.obs;
+
+  final screenList = [
+    const Text(
+      'Home',
+    ),
+    const Text(
+      'Store',
+    ),
+    const Text(
+      'Wishlist',
+    ),
+    const Text(
+      'Profile',
+    ),
+  ];
 }
