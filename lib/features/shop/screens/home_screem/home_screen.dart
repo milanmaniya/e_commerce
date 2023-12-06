e3de21366bf1de0b801386d9b4d18d8ec6a6bdb7
milanmaniya <milanmaniya2035@gmail.com>
2023-12-06 15:03:30 +0530

    done

--- lib/features/shop/screens/home_screem/home_screen.dart
+++ lib/features/shop/screens/home_screem/home_screen.dart
@@ -5,7 +5,7 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(),
       body: const SingleChildScrollView(
